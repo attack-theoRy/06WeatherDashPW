e386b36fbb5593a2c761b6fff9d9707d8154f1cb
Pirooz Wallace <PiroozWallace@outlook.com>
2020-11-04 15:57:24 -0800

    buggin
    ddd

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
 
 
 // get the city from input
-var citySearched = $("#searchTerm").val();
+var citySearched = $("#searchBox").val();
 
 // set my api Key
 var apiKey = "&appid=afaa8eea1769b4359fd8e07b2efcefbd";
