113834045c0619bdc96c5c74457951b7a507a187
Pirooz Wallace <PiroozWallace@outlook.com>
2020-11-05 10:47:02 -0800

    correcting variable names
    variable name changes

--- Assets/script.js
+++ Assets/script.js
@@ -14,14 +14,14 @@ var myQueryURL = ''
 $("#searchBtn").on("click", function() {
 
 // get the value from the form
-citySearched = $("#searchTerm").val();
+citySearched = $("#searchBox").val();
 
 // the full query 
 myQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearched + apiKey;
 
 // ajax api call
 $.ajax({
-  url: myQueryUrl,
+  url: myQueryURL,
   method: "GET"
 })
 .then(function (response){
@@ -31,7 +31,8 @@ $.ajax({
   console.log(response.name)
   console.log(response.weather[0].icon)
 
-  let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+  var tempFahren = (response.main.temp - 273.15) * 1.80 + 32;
+  
   console.log(Math.floor(tempF))
 
   console.log(response.main.humidity)
